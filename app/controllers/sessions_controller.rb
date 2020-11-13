d3fa33252be2a82dbf4c79849b3bad023e95b473
Bryn <bschulha@ualberta.ca>
2020-11-13 20:53:56 +0000

    Working through user auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  
+  before_filter :authorize
+
+  def new
+  end
+
+  def create
+    puts "*************"
+    puts params
+    puts "*************"
+    user = User.find_by_email(params[:email])
+    # If the user exists, and the password entered is correct
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie
+      # Logged in when they navigate around our website
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      # If users login doesn't work, send them back to the login form.
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+end
