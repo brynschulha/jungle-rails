1ab665b9072ef953be6e18ee983c627d61832882
Bryn <bschulha@ualberta.ca>
2020-11-19 08:04:16 +0000

    Final specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    user.email = email.downcase
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
